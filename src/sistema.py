3b06a26e4705c4ce67247e55fbb57d8199cd6147
Guillermo Berasategui Herranz <berasategui.116237@e.unavarra.es>
2020-11-25 09:13:00 +0100

    Creada matriz demandas

--- src/sistema.py
+++ src/sistema.py
@@ -7,9 +7,15 @@ estados = ['e', 'h', 's', 'v', 'g', 'c', 'b', 'x']
 
 class sistema():
 
-    def __init__(self,fA, fB):
+    def __init__(self, fA, fB):
         self.fA = fA
         self.fB = fB
+
+        self.crearMatricesProbabilidadTransición()
+        self.crearMatrizDemandas()
+
+        self.calcularFrecuenciaOperaciones()
+        self.calcularCargaOperaciones
 
     def crearMatricesProbabilidadTransición(self):
         # Mariz de Probabilidad de Transición de usuarios tipo A
@@ -39,34 +45,44 @@ class sistema():
         return
 
     def calcularFrecuenciaOperaciones(self):
-        #Calculo frecuencia de operaciones para usuario tipo A
+        # Calculo frecuencia de operaciones para usuario tipo A
         A = self.p_A.to_numpy().transpose()
         diagonal_A = np.diag(self.p_A) - 1
         np.fill_diagonal(A, diagonal_A)
-        A[0,0] = 1
+        A[0, 0] = 1
         B = np.zeros([len(estados)])
         B[0] = 1
-        self.v_A = np.linalg.solve(A,B)[:-1]
+        self.v_A = np.linalg.solve(A, B)[:-1]
 
-        #Calculo frecuencia de operaciones para usuario tipo B
+        # Calculo frecuencia de operaciones para usuario tipo B
         A = self.p_B.to_numpy().transpose()
         diagonal_A = np.diag(self.p_B) - 1
         np.fill_diagonal(A, diagonal_A)
-        A[0,0] = 1
+        A[0, 0] = 1
         B = np.zeros([len(estados)])
         B[0] = 1
-        self.v_B = np.linalg.solve(A,B)[:-1]
+        self.v_B = np.linalg.solve(A, B)[:-1]
         return
 
     def calcularCargaOperaciones(self):
         carga_media = self.v_A*self.fA + self.v_B*self.fB
-        self.carga_media = pd.DataFrame(data= carga_media, index=estados[:-1], columns=['carga'], dtype=float)
+        self.carga_media = pd.DataFrame(
+            data=carga_media, index=estados[:-1], columns=['carga'], dtype=float)
         print(self.carga_media)
         return
 
+    def crearMatrizDemandas(self):
+        K = ['WS-cpu', 'WS-disk', 'AS-cpu', 'AS-disk', 'DS-cpu', 'DS-disk']
+        R = ['h', 's', 'g', 'v', 'c', 'b']
+        self.D = pd.DataFrame(index=K, columns=R, dtype=float)
+        self.D.loc['WS-cpu'] = [0.008, 0.009, 0.011, 0.060, 0.012, 0.015]
+        self.D.loc['WS-disk'] = [0.03, 0.01, 0.01, 0.01, 0.01, 0.01]
+        self.D.loc['AS-cpu'] = [0.0, 0.03, 0.035, 0.025, 0.045, 0.04]
+        self.D.loc['AS-disk'] = [0.0, 0.008, 0.08, 0.009, 0.011, 0.012]
+        self.D.loc['DS-cpu'] = [0.0, 0.01, 0.009, 0.015, 0.07, 0.045]
+        self.D.loc['DS-disk'] = [0.0, 0.035, 0.018, 0.05, 0.08, 0.09]
+        print(self.D)
+        return
 
 
 sist = sistema(0.25, 0.75)
-sist.crearMatricesProbabilidadTransición()
-sist.calcularFrecuenciaOperaciones()
-sist.calcularCargaOperaciones()
